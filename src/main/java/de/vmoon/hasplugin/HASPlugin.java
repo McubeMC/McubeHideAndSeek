058d93874a618a59d88ba312b57b01d3cb1cc97e
VoidableMoon884 <trash_02@nogacity.de>
2024-03-03 12:18:18 +0100

    Changes for 2.4.3

--- src/main/java/de/vmoon/hasplugin/HASPlugin.java
+++ src/main/java/de/vmoon/hasplugin/HASPlugin.java
@@ -8,7 +8,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HASPlugin extends JavaPlugin {
     private static HASPlugin plugin;
-    private pvpCommand pvpCommand;
 
     @Override
     public void onEnable() {
@@ -18,7 +17,6 @@ public final class HASPlugin extends JavaPlugin {
         getCommand("has").setExecutor(new HasCommand());
         getCommand("pvp").setExecutor(new pvpCommand());
         getCommand("hashelp").setExecutor(new helpCommand());
-        pvpCommand.disablepvp();
 
     }
 
